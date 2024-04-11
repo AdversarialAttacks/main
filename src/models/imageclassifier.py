afbee765a3c31ac0ee1754272bb99164065606b7
Gabriel Torres Gamez <gabriel.torres@fuet.ch>
2024-04-11 15:52:42 +0200

    add epoch model unfreeze functionality

--- src/models/imageclassifier.py
+++ src/models/imageclassifier.py
@@ -8,7 +8,14 @@ from src.utils.metrics import metrics
 
 class ImageClassifier(L.LightningModule):
     def __init__(
-        self, modelname, output_size, p_dropout_classifier, lr=0.01, weight_decay=0
+        self,
+        modelname,
+        output_size,
+        p_dropout_classifier,
+        lr=0.01,
+        weight_decay=0,
+        first_unfreeze_epoch=0,
+        second_unfreeze_epoch=0,
     ):
         super().__init__()
         self.modelname = modelname
@@ -16,6 +23,9 @@ class ImageClassifier(L.LightningModule):
         self.p_dropout_classifier = p_dropout_classifier
         self.lr = lr
         self.weight_decay = weight_decay
+        self.first_unfreeze_epoch = first_unfreeze_epoch
+        self.second_unfreeze_epoch = second_unfreeze_epoch
+
         self.resize = None
 
         self.metrics = metrics
@@ -40,7 +50,7 @@ class ImageClassifier(L.LightningModule):
 
             # unfreeze all the layers (they probably already are xD)
             for param in self.model.parameters():
-                param.requires_grad = True
+                param.requires_grad = False
 
             # replace the last classifier layer on alexnet
             if modelname.startswith("alexnet"):
@@ -52,9 +62,7 @@ class ImageClassifier(L.LightningModule):
             # replace the last classifier layer on vgg
             elif modelname.startswith("vgg"):
                 self.model.classifier[-2] = torch.nn.Dropout(p=p_dropout_classifier)
-                self.model.classifier[-1] = torch.nn.Linear(
-                    self.model.classifier[-1].in_features, output_size
-                )
+                self.model.classifier[-1] = torch.nn.Linear(self.model.classifier[-1].in_features, output_size)
 
             # replace the fc layer on resnet
             elif modelname.startswith("resnet"):
@@ -90,8 +98,25 @@ class ImageClassifier(L.LightningModule):
                     f"Learning on Model {modelname} not implemented! Please choose between alexnet, vgg, resnet, densenet, efficientnet_v2 or vit."
                 )
 
+            self.layers = list(self.model.children())
+            self.total_layers = len(self.layers)
+
+            if self.first_unfreeze_epoch == 0:
+                self.unfreeze_layers(0.5)
+                print("Unfreezing 50% of the layers")
+
+            if self.second_unfreeze_epoch == 0:
+                self.unfreeze_layers(1.0)
+                print("Unfreezing 100% of the layers")
+
         except Exception as e:
             raise ValueError(f"Cannot load model {modelname}!") from e
+
+    def unfreeze_layers(self, percentage):
+        num_layers_to_unfreeze = int(self.total_layers * percentage)
+        for layer in self.layers[-num_layers_to_unfreeze:]:
+            for param in layer.parameters():
+                param.requires_grad = True
 
     def forward(self, x):
         if self.resize:
@@ -145,6 +170,14 @@ class ImageClassifier(L.LightningModule):
 
     def on_train_epoch_end(self):
         self.__on_epoch_end("train")
+
+        if (self.current_epoch - 1) == self.first_unfreeze_epoch:
+            self.unfreeze_layers(0.5)
+            print("Unfreezing 50% of the layers")
+
+        if (self.current_epoch - 1) == self.second_unfreeze_epoch:
+            self.unfreeze_layers(1.0)
+            print("Unfreezing 100% of the layers")
 
     def on_validation_epoch_end(self):
         self.__on_epoch_end("val")
