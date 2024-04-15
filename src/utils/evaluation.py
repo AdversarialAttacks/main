269c8e32620e959f25ded4b8e0c31c2ed19ae7ea
Ben <si_ben_tran@hotmail.com>
2024-04-15 16:25:38 +0200

    add get best epoch runs

--- src/utils/evaluation.py
+++ src/utils/evaluation.py
@@ -11,56 +11,11 @@ class WeightsandBiasEval:
         """
         self.api = wandb.Api()
         self.project_name = entity_project_name
-        self.runs_df = None
-        self.config = None
-        self.w_b_df = None
 
-    def fetch_runs(self):
-        """
-        Fetches all runs from the specified project and constructs a DataFrame.
-        """
-        runs = self.api.runs(self.project_name)
-        summary_list, config_list, name_list, id_list = [], [], [], []
+        self.last_runs = None
+        self.best_epochs = None
 
-        for run in runs:
-            summary_list.append(run.summary._json_dict)
-            config_list.append({k: v for k, v in run.config.items() if not k.startswith("_")})
-            name_list.append(run.name)
-            id_list.append(run.id)
-
-        self.runs_df = pd.DataFrame({"summary": summary_list, "config": config_list, "name": name_list, "id": id_list})
-
-    def save_runs_to_csv(self, path="src/data", filename="project", format="csv"):
-        """
-        Saves the runs DataFrame to a CSV file.
-
-        :param filename: A string specifying the filename for the CSV.
-        """
-        if self.runs_df is not None:
-            self.runs_df.to_csv(f"{path}/{filename}.{format}")
-        else:
-            print("No runs data to save. Please fetch the runs first by using method fetch_runs()")
-
-    def get_full_config_df(self):
-        """
-        Constructs and returns a full DataFrame of all run configurations.
-        """
-        if self.runs_df is None:
-            raise ValueError("No runs data available. Please fetch the runs first.")
-
-        self.config = pd.concat([pd.DataFrame([d]) for d in self.runs_df["config"]], ignore_index=True)
-        return self.config
-
-    def get_full_metrics_df(self):
-        """
-        Constructs and returns a full Dataframe of all run metrics
-        """
-        if self.runs_df is None:
-            raise ValueError("No runs data available. Please fetch the runs first.")
-
-        config_df = self.get_full_config_df()
-
-        metrics = [
+        self.metrics = [
             "train_BinaryAUROC",
             "train_BinaryAccuracy",
             "train_BinaryF1Score",
@@ -77,34 +32,89 @@ class WeightsandBiasEval:
             "val_loss",
         ]
 
-        result_df = pd.DataFrame(columns=metrics)
+        self.fetch_runs()
+        self.get_config()
+
+    def fetch_runs(self):
+        """
+        Fetches all runs from the specified project and constructs a DataFrame.
+        """
+        runs = self.api.runs(self.project_name)
+        summary_list, config_list, name_list, id_list = [], [], [], []
+
+        for run in runs:
+            summary_list.append(run.summary._json_dict)
+            config_list.append(
+                {k: v for k, v in run.config.items() if not k.startswith("_")}
+            )
+            name_list.append(run.name)
+            id_list.append(run.id)
+
+        self.runs_df = pd.DataFrame(
+            {
+                "summary": summary_list,
+                "config": config_list,
+                "name": name_list,
+                "id": id_list,
+            }
+        )
+
+    def get_config(self):
+        """
+        Constructs and returns a full DataFrame of all run configurations.
+        """
+        self.config = pd.concat(
+            [pd.DataFrame([d]) for d in self.runs_df["config"]], ignore_index=True
+        )
+        self.config["id"] = self.runs_df["id"]
+        return self.config
+
+    def get_last_runs(self):
+        """
+        Constructs and returns the last runs for each configuration.
+        """
+        result_df = pd.DataFrame(columns=self.metrics)
 
         for i in range(len(self.runs_df)):
             first_row = self.runs_df["summary"].iloc[i]
 
-            # create a dict with key (metric) and its value in first_row
             dict = {}
-            for metric in metrics:
+            for metric in self.metrics:
                 dict[metric] = first_row[metric]
 
-            # turn dict into dataframe
             temp_df = pd.DataFrame(dict, index=[0])
 
-            # concat to result_df and emptu temp_df
             result_df = pd.concat([result_df, temp_df], ignore_index=True)
 
-        # concat result_df with config_df
-        result_df = pd.concat([config_df, result_df], axis=1)
+        self.last_runs = pd.concat([self.config, result_df], axis=1)
 
-        self.w_b_df = result_df
-        return self.w_b_df
+        return self.last_runs
 
-    def get_best_run(self, sweep_id):
+    def get_best_epoch(self):
         """
-        Returns the best run from a specified sweep.
+        Constructs and returns the best runs for each configuration at lowest val_loss.
         """
-        if sweep_id is None:
-            raise ValueError("Sweep ID is not set. Please provide a sweep ID during initialization.")
 
-        sweep = self.api.sweep(f"{self.project_name}/{sweep_id}")
-        return sweep.best_run()
+        best_epochs = pd.DataFrame(columns=self.metrics + ["epoch"])
+
+        results = [self._get_best_epoch(run) for run in self.runs_df["id"]]
+        for result in results:
+            best_epochs = pd.concat([best_epochs, result], ignore_index=True)
+
+        self.best_epochs = pd.merge(self.config, best_epochs, on="id", how="outer")
+
+        return self.best_epochs
+
+    def _get_best_epoch(self, run):
+
+        run_hist = self.api.run(f"{self.project_name}/{run}").history()
+        run_hist = run_hist[self.metrics + ["epoch"]]
+        run_hist["id"] = run
+
+        return (
+            run_hist.groupby("epoch")
+            .first()
+            .sort_values("val_loss")
+            .head(1)
+            .reset_index()
+        )
